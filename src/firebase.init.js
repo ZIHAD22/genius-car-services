ba265631ecc6778be579cf2bcd0c07265caa6825
ZIHAD22 <mdzihad2244@gmail.com>
2022-04-25 16:22:42 +0600

    copy hole webiste

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBIbpj9Bh08_WlIirkibcuGY9w6Fb4Y39o',
-  authDomain: 'genius-car-services-59718.firebaseapp.com',
-  projectId: 'genius-car-services-59718',
-  storageBucket: 'genius-car-services-59718.appspot.com',
-  messagingSenderId: '324791431992',
-  appId: '1:324791431992:web:eaeda118d66d387e8787cd',
-}
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
